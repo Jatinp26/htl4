bac2dcb735f9eb8f3e903d85b5805790f4f4f5c5
Rohan Kulkarni <rohank2502@gmail.com>
2024-10-31 18:31:19 +0530

    Text Content Updated.

--- components/home/LegacySection.tsx
+++ components/home/LegacySection.tsx
@@ -2,13 +2,16 @@ import Section from "../common/Section";
 import LegacyCards from "../LegacyCards";
 
 export default function LegacySection() {
-
-    return (
-        <Section id="legacy">
-            <h2 className="text-white text-center font-semibold text-2xl sm:text-4xl lg:text-4xl mb-6 sm:mb-12 lg:mb-12">
-                The of <span className="bg-gradient-text bg-clip-text text-transparent">Legacy</span> Hack The League
-            </h2>
-            <LegacyCards />
-        </Section>
-    );
+  return (
+    <Section id="legacy">
+      <h2 className="text-white text-center font-semibold text-2xl sm:text-4xl lg:text-4xl mb-6 sm:mb-12 lg:mb-12">
+        Trip Down the{" "}
+        <span className="bg-gradient-text bg-clip-text text-transparent">
+          HTL
+        </span>{" "}
+        Lane
+      </h2>
+      <LegacyCards />
+    </Section>
+  );
 }
--- components/home/PresenceSection.tsx
+++ components/home/PresenceSection.tsx
@@ -13,11 +13,10 @@ export default function PresenceSection() {
   return (
     <Section id="presence">
       <h2 className="text-center text-white font-semibold mb-6 text-2xl sm:mb-12 sm:text-4xl lg:mb-12 lg:text-4xl">
-        The Most{" "}
+        Expanding{" "}
         <span className="bg-gradient-text bg-clip-text text-transparent">
-          Global
-        </span>{" "}
-        Presence
+          Horizons
+        </span>
       </h2>
       <div className="flex w-full flex-col gap-6 max-sm:gap-3">
         <div className="flex flex-col lg:flex-row gap-6 max-sm:gap-3 w-full">
@@ -33,12 +32,12 @@ export default function PresenceSection() {
               </div>
               <div className="flex flex-col gap-1">
                 <h2 className="text-violet-600 font-semibold text-2xl sm:text-3xl lg:text-3xl">
-                  Collaborated with Various Events
+                  Our Global Presence
                 </h2>
                 <p className="text-white text-[10px] sm:text-xs lg:text-xs">
-                  We&apos;ve partnered with leading global events like DEVCON,
-                  WebX 2024, DAO Tokyo, and many more, driving innovation in
-                  decentralized technologies.
+                  Partnering with leading global events like DEVCON, WebX 2024,
+                  DAO Tokyo and many more strengthened the community of Web3
+                  globally.
                 </p>
               </div>
             </div>
@@ -57,32 +56,32 @@ export default function PresenceSection() {
                 </div>
                 <div className="flex flex-col gap-1">
                   <h2 className="text-[#19FB9B] font-semibold text-2xl sm:text-3xl lg:text-3xl">
-                    Increase in Numbers
+                    Let the numbers speak!
                   </h2>
                   <p className="text-white text-[10px] sm:text-xs lg:text-xs">
-                    These numbers reflect our expanding influence, global reach,
-                    and the trust we&apos;ve built within the community and
-                    industry.
+                    The unmeasurable trust and passion that we&apos;ve built
+                    over the three chapters, is what drives humans of HTL to
+                    push beyond our limits!
                   </p>
                 </div>
               </div>
               <div className="h-[0.5px] w-full border border-[#2c2c2c]" />
-              <div className="flex flex-col">
+              <div className="flex flex-col gap-2">
                 <div>
                   <h2 className="text-[#19FB9B] font-semibold text-2xl">
-                    20k +
+                    98k++
                   </h2>
                   <p className="text-xs text-white">Social Media Reach</p>
                 </div>
                 <div>
                   <h2 className="text-[#19FB9B] font-semibold text-2xl">
-                    500 +
+                    2500++
                   </h2>
                   <p className="text-xs text-white">Followers</p>
                 </div>
                 <div>
                   <h2 className="text-[#19FB9B] font-semibold text-2xl">
-                    20 +
+                    37++
                   </h2>
                   <p className="text-xs text-white">
                     Trusted partners & sponsors
@@ -102,14 +101,16 @@ export default function PresenceSection() {
                 color="#2DB8EA"
               />
             </div>
-            <div className="flex flex-col gap-1">
+            <div className="flex flex-col gap-4">
               <h2 className="text-[#2DB8EA] font-semibold text-2xl sm:text-3xl lg:text-3xl">
                 Expanding Global Footprint
               </h2>
-              <p className="text-white text-[10px] sm:text-xs lg:text-xs">
-                With a presence in 5+ countries, we’re driving innovation in
-                decentralized technologies and shaping the future of the digital
-                world.
+              <p className="text-white text-[10px] sm:text-xs lg:text-xs  tracking-wider">
+                Beyond hackathons, HTL is a haven for those who want to explore
+                the core of Web3 and AI technologies with bright people
+                worldwide. With the prominent presence in 5+ countries we aim to
+                keep innovation at the core to shape the future of decentralized
+                technologies.
               </p>
             </div>
           </div>
@@ -124,7 +125,7 @@ export default function PresenceSection() {
                 />
               </div>
               <h2 className="text-[#f3ec40] font-semibold text-2xl sm:text-3xl lg:text-3xl">
-                Glimpse from the Events
+                Glimpse To Recollect
               </h2>
             </div>
             <EventGlimpseSlider />
