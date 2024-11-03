2782d94419bb318ee6573695e18e5aa29c2380a9
Rohan Kulkarni <rohank2502@gmail.com>
2024-11-03 15:08:04 +0530

    Updated images.

--- components/EventGlimpseSlider.tsx
+++ components/EventGlimpseSlider.tsx
@@ -2,27 +2,45 @@ import Image from "next/image";
 import Marquee from "@/components/magicui/marquee";
 
 export default function EventGlimpseSlider() {
-    const eventImages = [
-        { src: "/images/eventImages/Event-1.webp", alt: "Event-1" },
-        { src: "/images/eventImages/Event-2.webp", alt: "Event-2" },
-        { src: "/images/eventImages/Event-3.webp", alt: "Event-3" },
-    ];
+  const eventImages = [
+    {
+      src: "https://res.cloudinary.com/hack-the-league/image/upload/v1730386923/website/global%20presence/IMG20240316162628_agcz2x.jpg",
+      alt: "Event-1",
+    },
+    {
+      src: "https://res.cloudinary.com/hack-the-league/image/upload/v1730386921/website/global%20presence/GWnNlZwbgAAzy7x_bqraaq.jpg",
+      alt: "Event-2",
+    },
+    {
+      src: "https://res.cloudinary.com/hack-the-league/image/upload/v1730386920/website/global%20presence/GWnO4moa4AAK209_ospv5t.jpg",
+      alt: "Event-3",
+    },
+    {
+      src: "https://res.cloudinary.com/hack-the-league/image/upload/v1730386918/website/global%20presence/GT9r2OQW8AAwe3l_etzsyy.jpg",
+      alt: "Event-3",
+    },
+    {
+      src: "https://res.cloudinary.com/hack-the-league/image/upload/v1730386917/website/global%20presence/GI7pcWSWkAAkS3D_f9kr3n.jpg",
+      alt: "Event-3",
+    },
+  ];
 
-    return (
-        <Marquee pauseOnHover={false} className="[--duration:30s]">
-            {eventImages.map((image, index) => (
-                <div 
-                    key={index} 
-                    className="flex items-center gap-1 justify-center h-[205.5px]">
-                    <Image
-                        src={image.src}
-                        alt={image.alt}
-                        width={400}
-                        height={300}
-                        className="object-contain max-sm:rounded-sm rounded-md h-full w-auto"
-                    />
-                </div>
-            ))}
-        </Marquee>
-    );
+  return (
+    <Marquee pauseOnHover={false} className="[--duration:30s]">
+      {eventImages.map((image, index) => (
+        <div
+          key={index}
+          className="flex items-center gap-1 justify-center h-[205.5px]"
+        >
+          <Image
+            src={image.src}
+            alt={image.alt}
+            width={400}
+            height={300}
+            className="object-contain max-sm:rounded-sm rounded-md h-full w-auto"
+          />
+        </div>
+      ))}
+    </Marquee>
+  );
 }
--- components/LegacyCards.tsx
+++ components/LegacyCards.tsx
@@ -9,29 +9,30 @@ export default function LegacyCards() {
       title: "Chapter-1",
       subtitle:
         "Where the aim for creating an impact-driven community was born!",
-      img1: Image1,
-      img2: Image2,
+      img1: "https://res.cloudinary.com/hack-the-league/image/upload/v1730386916/website/chapter1/1_h3vkmx.png",
+      img2: "https://res.cloudinary.com/hack-the-league/image/upload/v1730386913/website/chapter1/2_bpjls8.png",
+      img4: "https://res.cloudinary.com/hack-the-league/image/upload/v1730386915/website/chapter1/3_khfska.png",
     },
     {
       title: "Chapter-2",
       subtitle:
         "An in-person hackathon that gathered like-minded people to craft wonders using technologies.",
-      img1: Image1,
-      img2: Image2,
+      img1: "https://res.cloudinary.com/hack-the-league/image/upload/v1730386963/website/chapter2/Hacktheleagu420_cklgdl.jpg",
+      img2: "https://res.cloudinary.com/hack-the-league/image/upload/v1730386934/website/chapter2/Hacktheleagu387_trvn95.jpg",
     },
     {
       title: "Meetup-Series",
       subtitle:
         "6 City meet-ups packed by powerful speakers and workshop sessions created a stir among hackers!",
-      img1: Image1,
-      img2: Image2,
-      img3: Image1,
+      img1: "https://res.cloudinary.com/hack-the-league/image/upload/v1730625479/website/meetups/sdwzarajvjib8agooak8.png",
+      img2: "https://res.cloudinary.com/hack-the-league/image/upload/v1730386931/website/meetups/IMG20221113161402_yspe8l.jpg",
+      img3: "https://res.cloudinary.com/hack-the-league/image/upload/v1730625477/website/meetups/knqgahmefdcip11tm48a.png",
     },
     {
       title: "Chapter-3",
       subtitle: "An Aim for Global Impact",
-      img1: Image1,
-      img2: Image2,
+      img1: "https://res.cloudinary.com/hack-the-league/image/upload/v1730387026/website/chapter%203/ab3223a2-9613-4e18-8d79-80407bf8546f.png",
+      img2: "https://res.cloudinary.com/hack-the-league/image/upload/v1730386933/website/chapter%203/HTL-430_yqx6am.jpg",
     },
   ];
   return (
@@ -41,15 +42,42 @@ export default function LegacyCards() {
         {events.map((event, index) => (
           <div key={index} className="flex gap-4">
             {/* Left Image */}
-            <div className="rounded-lg overflow-hidden shadow-lg max-sm:rounded-md  relative max-sm:w-[205.5px] max-sm:h-[205.5px] w-[411px] h-[411px]">
+            <div className="rounded-lg overflow-hidden shadow-lg max-sm:rounded-md  relative max-sm:w-[205.5px] max-sm:h-[205.5px] w-[411px] h-[411px] flex flex-col gap-6">
+              {event.title == "Chapter-1" ? (
+                <div className="h-full flex-col gap-6">
+                  <Image
+                    src={event.img1}
+                    alt={`Event Image 1 for ${event.title}`}
+                    className="w-full h-1/2 mb-4 aspect-video rounded-lg"
+                    width={100}
+                    height={100}
+                  />
+                  <Image
+                    src={event?.img4 || ""}
+                    alt={`Event Image 4 for ${event.title}`}
+                    className="h-1/2 w-full aspect-video rounded-lg"
+                    width={100}
+                    height={100}
+                  />
+                </div>
+              ) : (
+                <Image
+                  src={event.img1}
+                  alt={`Event Image 1 for ${event.title}`}
+                  layout="fill"
+                  objectFit="cover"
+                  className="rounded-xl max-sm:rounded-md "
+                />
+              )}
               <Image
                 src={event.img1}
                 alt={`Event Image 1 for ${event.title}`}
-                layout="fill"
-                objectFit="cover"
-                className="rounded-xl max-sm:rounded-md "
+                className="w-full h-1/2"
+                width={400}
+                height={499}
               />
             </div>
+
             {/* Right Section with Image 2 and Text */}
             <div className="relative max-sm:w-[205.5px] max-sm:h-[205.5px] w-[411px] h-[411px] flex flex-col items-center gap-6 max-sm:gap-3">
               <div className="h-1/2 w-full rounded-lg max-sm:rounded-md  overflow-hidden shadow-lg relative">
